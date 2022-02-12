d04d7319033ecfa300af3c92700c1c7372e35b19
RamonaStanberry <monacstanberry@gmail.com>
2022-02-12 11:42:47 -0500

    Edited profile information

--- app/views.py
+++ app/views.py
@@ -27,7 +27,7 @@ def home():
 @app.route('/about/')
 def about():
     """Render the website's about page."""
-    return render_template('about.html', name="Mary Jane")
+    return render_template('about.html', name="Ramona Stanberry")
 
 @app.route('/profile')
 def profile():
